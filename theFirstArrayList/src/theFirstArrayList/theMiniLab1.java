a6b3265c62d0912264fb82b8a423eb2ace0d3ae7
michaelceretop <zixian.chen@stjlabs.com>
2019-02-12 10:51:17 -0500

    Started the mini lab 1

---
+++ theFirstArrayList/src/theFirstArrayList/theMiniLab1.java
@@ -0,0 +1,19 @@
+package theFirstArrayList;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class theMiniLab1 {
+	public static void main(String[] args)
+	{
+		theFactors();
+	}
+	
+	public static void theFactors()
+	{
+		
+		ArrayList <Integer> factors = new ArrayList <Integer> ();
+		
+	}
+}
